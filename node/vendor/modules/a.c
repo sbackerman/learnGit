1f7988d115befbb746b583f9d533cc355b6ac93e
qingquan <qingquan@inwil.com>
2021-03-10 12:37:57 +0800

    node vendor

---
+++ node/vendor/modules/a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("node vendor a.c \n");
+	
+	return 0;
+}
+
+
---
+++ vendor/modules/a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello, world a.c \n");
+	
+	return 0;
+}
+
+
