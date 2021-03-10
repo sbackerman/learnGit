62133ca1fdc42d5c0cb61db95aacc1545a04c0c6
qingquan <qingquan@inwil.com>
2021-03-10 19:24:04 +0800

    输出日志

--- webhook.php
+++ webhook.php
@@ -72,7 +72,10 @@ $isMatched = gitee($secret);
 
 
 if($isMatched) {
-	echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+	$cmd = "cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1" ;
+	$ret =  shell_exec($cmd);
+	file_put_contents('/tmp/learnGit.log', $ret ,FILE_APPEND);
+	echo $ret ;
 	exit();
 } else {
 	http_response_code(404);
