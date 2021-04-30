f48e735d55f8c68e5e5181db5fbde8e3c8a9afb8
qingquan <qingquan@inwil.com>
2021-04-30 11:31:21 +0800

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 echo '刘阿勇 v6 aliyun ' . date("Y-m-d H:i:s") . "<br>" ;
-echo '刘阿勇 v6 aliyun ' . date("Y-m-d H:i:s") . "<br>" ;
+echo '刘阿勇 v7 thinkpad ' . date("Y-m-d H:i:s") . "<br>" ;
 
 ?>
