0dbcbae4a17b4321d843905c5314a26665c3299d
qingquan <qingquan@inwil.com>
2021-04-30 11:46:39 +0800

    shell.php

--- shell.php
+++ shell.php
@@ -1,13 +1,21 @@
 <?php
 
 // 项目地址
-$path = "/home/wwwroot/learnGit";
+$path = "/home/wwwroot/learnGit" ;
+
 $cmd  = "cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1" ;
+
 $ret  =  shell_exec($cmd);
-file_put_contents('/tmp/learnGit.log', $ret ,FILE_APPEND);
+
+file_put_contents('/tmp/learnGit.log', $ret ."\n" ,FILE_APPEND);
+
 echo $cmd . "\n" ;
 echo $ret . "\n" ;
+
+
 exit('done');
+
+
 
 
 
